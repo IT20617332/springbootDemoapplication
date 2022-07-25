fbf522aabece76c297dd3596cd52bffc909be228
IT20617332 <siridana@gmail.com>
2022-07-25 16:49:07 +0530

    ffdf

--- src/main/java/com/example/demoapplication/repo/UserRepo.java
+++ src/main/java/com/example/demoapplication/repo/UserRepo.java
@@ -12,5 +12,5 @@ public interface UserRepo extends JpaRepository<User, Integer> {
     @Query(value = "SELECT * FROM USER WHERE ID = ?1 AND ADDRESS = ?2",nativeQuery = true)//native query search by id and address
     User getUserByUserIDAndAddress(String userId, String address);
 
-    //@Modyfying for update query instead of select
+    //@Modyfying for update query instead of selected
 }
